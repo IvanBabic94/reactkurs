37562ca12b458b92265fba00bfc8c42c0b2b24d8
IvanBabic94 <babsonno2@gmail.com>
2024-08-29 14:24:41 +0200

    update 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import { useState } from 'react';
 
 import { CORE_CONCEPTS } from './data.js';
 
-import Header from './components/Header.jsx';
+import Header from './components/Header/Header.jsx';
 import CoreConcept from './components/CoreConcept.jsx';
+import TabButton from './components/TabButton.jsx';
 
 
 // function CoreConcept(props) {
@@ -25,7 +27,17 @@ import CoreConcept from './components/CoreConcept.jsx';
 //   );
 // }
 
+
+
 function App() {
+  const [ selectedTopic, setSelectedTopic ] = useState('Please click a button');
+
+  function handleSelect (selectedButton) {
+    // selectedButton => Components, JSX, Props State
+    setSelectedTopic(selectedButton);
+};
+
+
   return (
     <div>
       <Header />
@@ -38,6 +50,16 @@ function App() {
           <CoreConcept {...CORE_CONCEPTS[2]} />
           <CoreConcept {...CORE_CONCEPTS[3]} />
         </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton onSelect={() => handleSelect('components')}>Components</TabButton>
+            <TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
+          </menu>
+          {selectedTopic}
         </section>
         <h2>Time to get started!</h2>
       </main>
---
+++ src/components/TabButton.jsx
@@ -0,0 +1,6 @@
+export default function TabButton (props) {   
+    return (
+        <li>
+        <button onClick={props.onSelect} >{props.children}</button>
+        </li>)
+}
